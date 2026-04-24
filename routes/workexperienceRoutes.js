db503b3691bea31b9e63e166c33e19f53a3f697b
Coco10023 <mauricio2003@live.se>
2026-04-24 18:06:30 +0200

    Skriver kod för routes/workexperienceRoutes.js

---
+++ routes/workexperienceRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express"); 
+const router = express.Router();
+const controller = require("../controllers/workexperienceController");
+
+router.get("/", controller.getAllExperiences);
+router.get("/:id", controller.getExperienceById);
+router.post("/", controller.updateExperience);
+router.put("/:id", controller.updateExperience);
+router.delete("/:id", controller.deleteExperience);
+
+module.exports = router; 
